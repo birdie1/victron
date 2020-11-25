564b486cd77bc71f781f5bde11da281c63629eaf
Martin Haaß <vvvrrooomm@gmail.com>
2020-11-25 11:32:12 +0100

    restructure code to use table per category, start add smartsolar

--- victron.lua
+++ victron.lua
@@ -12,13 +12,27 @@ fields.remaining   = ProtoField.uint8("victron.remaining", "Remainig pkts", base
 fields.protocol_type   = ProtoField.uint8("victron.protocol_type", "prot type", base.HEX)
 
 local value_types = {
-	[0x8c] = "Current",
-	[0x8d] = "Voltage",
-	[0x8e] = "Power",
-	--[0x8f] = "Capacity",
-	[0x7d] = "Starter",
-	
+	[0x8c] = "SmartShunt Current",
+	[0x8d] = "Victron Voltage",
+	[0x8e] = "Victron Power",
+	[0x7d] = "SmartShunt Starter",
+	[0x8f] = "SmartSolar Battery Current",
 }
+fields.current   = ProtoField.float("victron.current", value_types[0x8c], {" A"},  base.UNIT_STRING)
+fields.voltage   = ProtoField.float("victron.voltage", value_types[0x8d], {" V"},  base.UNIT_STRING)
+fields.power   = ProtoField.float("victron.power", value_types[0x8e], {" w"}, base.UNIT_STRING)
+fields.starter   = ProtoField.float("victron.starter", value_types[0x7d], {"V"}, base.UNIT_STRING)
+fields.battery_current   = ProtoField.float("victron.battery_current", value_types[0x8f], {"A"}, base.UNIT_STRING)
+local value_commands = {
+	[0x8c] = {fields.current,1000},
+	[0x8d] = {fields.voltage,100},
+	[0x8e] = {fields.power,1}, --smartshunt
+	[0x7d] = {fields.starter,100},
+	[0x8f] = {fields.battery_current,10},
+	[0xbc] = {fields.power,100}, -- smartsolar
+}
+
+
 local command_categories = {
 	[0x03190308] = "history values",
 	[0x03190309] = "history bools",
@@ -35,30 +49,22 @@ local data_types = {
 	[0x09] = "bool (1byte fixed len)",
 }
 
-fields.state_of_charge = ProtoField.float("victron.state_of_charge", "Battery Charge Status", {" %"}, base.DEC or base.UNIT_STRING)
+local mixedsetting_types={
+[0xff] = "Battery Charge Status", 
+}
+fields.mixedsettings   = ProtoField.uint8("victron.mixedsettings", "mixed settings", base.HEX, mixedsettings_types)
+fields.state_of_charge = ProtoField.float("victron.state_of_charge", mixedsetting_types[0xff], {" %"}, base.DEC or base.UNIT_STRING)
 fields.bool_bat_start_sync = ProtoField.bool("victron.bat_start_sync", "battery starts synchronized")
-local mixed_settings = {
-	[0xfd] = fields.bool_bat_start_sync,
-	[0xff] = fields.state_of_charge,
+local mixedsetting_commands = {
+	[0xfd] = {fields.bool_bat_start_sync,1},
+	[0xff] = {fields.state_of_charge,100},
 }
 
 fields.command_category = ProtoField.uint32("victron.cmd_category", "command category", base.HEX, command_categories)
 fields.start_sequence   = ProtoField.uint32("victron.start_sequence", "start_squence", base.HEX)
 fields.data_type   = ProtoField.uint8("victron.data_type", "data type", base.HEX, data_types)
 fields.value   = ProtoField.uint8("victron.command", "command", base.HEX, value_types)
-local settings_types = {
-	[0x00] = "set capacity!",
-	[0x01] = "set charged voltage!",
-	[0x02] = "set tail current!",
-	[0x03] = "set charged detection time!",
-	[0x04] = "set charge eff. factor!",
-	[0x05] = "set peukert coefficient!",
-	[0x06] = "set current threshold",
-	[0x07] = "set time-to-go avg. per.",
-	[0x08] = "set discharge floor!",
-	[0xff] = "Battery Charge Status", -- this should go into a separate table for category mixed_settings
-}
-fields.settings_value   = ProtoField.uint8("victron.settings", "settings", base.HEX, settings_types)
+
 
 local hist_types = {
 	[0x00] = "hist: deepest discharge",
@@ -73,7 +79,6 @@ local hist_types = {
 	[0x09] = "hist: synchronizations",
 	[0x10] = "hist: Discharged Energy",
 	[0x11] = "hist: Charged Energy",
-	
 }
 fields.history = ProtoField.uint8("victron.history", "history", base.HEX, hist_types)
 fields.hist_synch = ProtoField.uint16("victron.hist_synch", hist_types[0x09], base.DEC)
@@ -88,9 +93,20 @@ fields.hist_max_bat = ProtoField.float("victron.hist_max_bat", hist_types[0x07],
 fields.hist_time_full = ProtoField.int32("victron.hist_time_full", hist_types[0x08])
 fields.hist_discharged_energy = ProtoField.float("victron.hist_discharged_energy",  hist_types[0x10], {" kWh"}, base.UNIT_STRING)
 fields.hist_charged_energy = ProtoField.float("victron.hist_charged_energy",  hist_types[0x11], {" kWh"}, base.UNIT_STRING)
-
-
-fields.settings_value   = ProtoField.uint8("victron.settings", "settings", base.HEX, settings_types)
+local hist_commands = {
+	[0x00] = {fields.hist_deepest_discharge,10},
+	[0x01] = {fields.hist_last_discharge,10},
+	[0x02] = {fields.hist_avrg_discharge,10},
+	[0x03] = {fields.hist_cycles,1},
+	[0x04] = {fields.hist_full_discharge,1},
+	[0x05] = {fields.hist_cum_drawn,10},
+	[0x06] = {fields.hist_min_bat,100},
+	[0x07] = {fields.hist_max_bat,100},
+	[0x08] = {fields.hist_time_full,1},
+	[0x09] = {fields.hist_synch,1},
+	[0x10] = {fields.hist_discharged_energy,100},
+	[0x11] = {fields.hist_charged_energy,100},
+ }
 
 
 command_class_type = { [0x0] = "status reply?", [0x4] ="data reply?", [0x8] = "bulk values??"}
@@ -106,31 +122,50 @@ fields.crc   = ProtoField.uint8("victron.crc", "crc", base.HEX)
 fields.reserved   = ProtoField.uint8("victron.reserved", "Reserved", base.HEX)
 
 
-fields.voltage   = ProtoField.uint16("victron.voltage", "voltage", base.HEX)
-fields.current   = ProtoField.int32("victron.current", "current", base.DEC)
-fields.power   = ProtoField.int16("victron.power", "power", base.DEC)
-fields.starter   = ProtoField.int16("victron.starter", "starter", base.DEC)
+
 --fields.capacity   = ProtoField.float("victron.capacity", "capacity (%)", base.UNIT_STRING, { [0]="%"})
 fields.capacity   = ProtoField.int32("victron.capacity", "capacity (Ah)")
-fields.set_capacity   = ProtoField.int32("victron.set_capacity", "set capacity (Ah)")
 
-fields.set_charged_volt  = ProtoField.float("victron.charged_voltage", "charged Voltage ", {" V"})
-fields.set_dis_floor  = ProtoField.uint16("victron.dis_floor", "discharge floor ", base.UNIT_STRING, {" %"})
-fields.set_tail_current  = ProtoField.float("victron.set_tail_current", "set tail current ", {" %"})
-fields.set_peukert  = ProtoField.float("victron.set_peukert", "set peukert coeff", {"%"})
-fields.set_curr_threshold= ProtoField.float("victron.set_curr_threshold", "set current threshold (A)", {[1]=" A"})
 
-fields.set_charged_time= ProtoField.uint16("victron.set_charged_time", "set charged det. time ", base.UNIT_STRING,{" min"})
-fields.set_eff_factor  = ProtoField.uint16("victron.set_eff_factor", "charge eff. factor ",base.UNIT_STRING or base.DEC ,{" %"})
-fields.set_timetogo  = ProtoField.uint16("victron.timetogo", "time-to-go avg. period", base.UNIT_STRING,{" min"})
+local settings_types = {
+	[0x00] = "set capacity!",
+	[0x01] = "set charged voltage!",
+	[0x02] = "set tail current!",
+	[0x03] = "set charged detection time!",
+	[0x04] = "set charge eff. factor!",
+	[0x05] = "set peukert coefficient!",
+	[0x06] = "set current threshold",
+	[0x07] = "set time-to-go avg. per.",
+	[0x08] = "set discharge floor!",
+}
+fields.settings_value   = ProtoField.uint8("victron.settings", "settings", base.HEX, settings_types)
+fields.set_capacity   = ProtoField.int32("victron.set_capacity", settings_types[0x00])
+fields.set_charged_volt  = ProtoField.float("victron.charged_voltage", settings_types[0x01], {" V"})
+fields.set_dis_floor  = ProtoField.uint16("victron.dis_floor", settings_types[0x08], base.UNIT_STRING, {" %"})
+fields.set_tail_current  = ProtoField.float("victron.set_tail_current", settings_types[0x02], {" %"})
+fields.set_peukert  = ProtoField.float("victron.set_peukert", settings_types[0x05], {"%"})
+fields.set_curr_threshold= ProtoField.float("victron.set_curr_threshold", settings_types[0x06], {[1]=" A"})
+fields.set_charged_time= ProtoField.uint16("victron.set_charged_time", settings_types[0x03], base.UNIT_STRING,{" min"})
+fields.set_eff_factor  = ProtoField.uint16("victron.set_eff_factor", settings_types[0x04],base.UNIT_STRING or base.DEC ,{" %"})
+fields.set_timetogo  = ProtoField.uint16("victron.timetogo", settings_types[0x07], base.UNIT_STRING,{" min"})
 fields.set_booltype  = ProtoField.uint8("victron.set_booltype", "bool type", base.HEX)
 fields.set_boolvalue  = ProtoField.bool("victron.set_boolvalue", "bool value")
-
+local settings_commands = {
+	[0x00] = {fields.set_capacity, 1},
+	[0x01] = {fields.set_charged_volt,10},
+	[0x02] = {fields.set_tail_current,10},
+	[0x03] = {fields.set_charged_time,1},
+	[0x04] = {fields.set_eff_factor,1},
+	[0x05] = {fields.set_peukert,100},
+	[0x06] = {fields.set_curr_threshold,100},
+	[0x07] = {fields.set_timetogo,1},
+	[0x08] = {fields.set_dis_floor,10},	
+}
 
 fields.unknown8   = ProtoField.uint8("victron.unknown8", "Unknown8 value", base.HEX)
 fields.unknown16   = ProtoField.uint16("victron.unknown16", "Unknown16 value", base.HEX)
 fields.unknown24   = ProtoField.uint24("victron.unknown24", "Unknown24 value", base.HEX)
-fields.unknown32   = ProtoField.uint32("victron.unknown32", "Unknown32 value", base.HEX)
+fields.unknown32   = ProtoField.int32("victron.unknown32", "Unknown32 value", base.DEC)
 fields.unknown_bool_type  = ProtoField.uint8("victron.unknown_bool_type", "unknwon bool type", base.HEX)
 fields.unknown_bool_value  = ProtoField.bool("victron.unknown_bool_value", "unknown bool value")
 
@@ -143,8 +178,6 @@ local statuses = {
 [0x05] = "Command Not Support",
 [0x82] = "Status Result",
 }
-
-
 
 function payload_dissector(buffer, pinfo, tree, size, command)
 	fun = (command_dissector[command] and command_dissector[command]) or default_command
@@ -163,105 +196,6 @@ function length_one(buffer, pinfo, subtree)
 	-- pinfo.cols.info = name
 	subtree:add_le(fields.reserved, buffer(0,1))
 
-end
-
-
-
-function voltage(buffer,pinfo,subtree)
-	local val = buffer(0,2):le_uint() / 100
-	subtree:add_le(fields.voltage, buffer(0,2)):append_text(": "..val.."V")
-	pinfo.cols.info = "voltage: " .. val
-	return 2
-end
-
-function current(buffer,pinfo,subtree)
-	local val = buffer(0,4):le_int() /1000
-	subtree:add_le(fields.current, buffer(0,4), val, "current"):append_text(": "..val.."A")
-	pinfo.cols.info = "current: " .. val .."A"
-	return 4
-end
-
-function power(buffer,pinfo,subtree)
-	local val = buffer(0,2):le_int() 
-	subtree:add_le(fields.power, buffer(0,2)):append_text(": "..val.."W")
-	pinfo.cols.info = "power: " .. val .."W"
-	return 2
-end
-
-function starter(buffer,pinfo,subtree)
-	local val = buffer(0,2):le_int() /100
-	subtree:add_le(fields.starter, buffer(0,2), val):append_text(": "..val.."V")
-	pinfo.cols.info = "starter: " .. val .. "V"
-	return 2
-end
-
--- function capacity(buffer,pinfo,subtree, text)
--- 	local val =  buffer(0,2):le_uint() 
--- 	local capa = val / 0xFFFF *100
--- 	subtree:add(fields.capacity, buffer(0,2), val )
--- 	pinfo.cols.info = text..": " .. val 
--- 	return 2
--- end
-
-function set_capacity(buffer,pinfo,subtree)
-	local val =  buffer(0,2):le_uint() 
-	subtree:add(fields.set_capacity, buffer(0,2), val )
-	pinfo.cols.info = "set capacity: " .. val 
-	return 2
-end
-
-function set_avg_period(buffer,pinfo,subtree)
-	subtree:add(fields.set_timetogo, buffer(0,2):le_uint())
-	pinfo.cols.info = "set time-to-go:" ..buffer(0,2):le_uint()
-	return 2
-end
-
-function set_dis_floor(buffer,pinfo,subtree)
-	subtree:add(fields.set_dis_floor, buffer(0,2):le_uint()/10)
-	pinfo.cols.info = "set discharge floor:" ..buffer(0,2):le_uint()/10
-	return 2
-end
-
-
-function set_charged_time(buffer,pinfo,subtree)
-	subtree:add(fields.set_charged_time, buffer(0,2):le_uint())
-	pinfo.cols.info = "set charged det. time:" ..buffer(0,2):le_uint()
-	return 2
-end
-
-function set_charged_volt(buffer,pinfo,subtree)
-	local  val = buffer(0,2):le_uint()/10.0
-	subtree:add(fields.set_charged_volt, buffer(0,2),val)
-	pinfo.cols.info = "set charged volt:" ..val
-	return 2
-end
-
-function set_eff_factor(buffer,pinfo,subtree)
-	local  val = buffer(0,2):le_uint()
-	subtree:add(fields.set_eff_factor,buffer(0,2), val)
-	pinfo.cols.info = "set charge eff. factor" ..val
-	return 2
-end
-
-function set_tail_current(buffer,pinfo,subtree)
-	local  val = buffer(0,2):le_uint() /10.0
-	subtree:add(fields.set_tail_current,buffer(0,2), val)
-	pinfo.cols.info = "set tail current" ..val
-	return 2
-end
-
-function set_peukert(buffer,pinfo,subtree)
-	local  val = buffer(0,2):le_uint() /100
-	subtree:add(fields.set_peukert,buffer(0,2), val)
-	pinfo.cols.info = "set peukert coeff" ..val
-	return 2
-end
-
-function set_curr_threshold(buffer,pinfo,subtree)
-	local  val = buffer(0,2):le_uint() /100
-	subtree:add(fields.set_curr_threshold,buffer(0,2), val)
-	pinfo.cols.info = "set current threshold" ..val
-	return 2
 end
 
 function add_unknown_bool(buffer,pinfo,subtree)
@@ -291,24 +225,7 @@ function add_unknown_field(buffer,pinfo,subtree)
 		return data_size_nibble
 end
 
-local value_types_functions = {
-	[0x8c] = current,
-	[0x8d] = voltage,
-	[0x8e] = power,
-	--[0x8f] = "Capacity",
-	[0x7d] = starter,
-	
-}
-
-function command_category(buffer, pinfo, subtree)
-	command_type = buffer(0,1):le_uint() 
-	command_fun = value_types_functions[command_type]
-	if command_fun == nil then
-		command_fun = add_unknown_field
-	end
-
-	return command_fun(buffer(2),pinfo,subtree)
-end
+local unknown_command = {fields.unknown32, 1}
 
 
 function settings_bool(buffer, pinfo, subtree)
@@ -317,67 +234,20 @@ function settings_bool(buffer, pinfo, subtree)
 	return 2
 end
 
-function settings_category(buffer, pinfo,subtree)
- local type_funcs = {
-		[0x00] = set_capacity,
-		[0x01] = set_charged_volt,
-		[0x02] = set_tail_current,
-		[0x03] = set_charged_time,
-		[0x04] = set_eff_factor,
-		[0x05] = set_peukert,
-		[0x06] = set_curr_threshold,
-		[0x07] = set_avg_period,
-		[0x08] = set_dis_floor,
-		
-		
-	}
-	settings_type = buffer(0,1):le_uint() 
-	fun = type_funcs[settings_type]
-	if fun == nil then
-		fun = add_unknown_field
+function command_category(buffer, pinfo, subtree, data_size, command_types)
+
+	command = buffer(0,1):le_uint() 
+	local fun
+	if command_types[command] == nil then
+		fun = unknown_command
+	else
+	 fun = command_types[command]
 	end
-	
-	return fun(buffer(2),pinfo,subtree)
+	local value = buffer(2,data_size):le_int() / fun[2] 
+	subtree:add_le(fun[1], buffer(2,data_size), value)
+	return data_size
 end
 
-function mixed_settings(buffer, pinfo, subtree)
-	local type = buffer(4,1):le_uint()
-
-	if type == 0xff then
-		local val = buffer(6,2):le_uint() / 100
-		subtree:add_le(fields.state_of_charge, buffer(6,2), val)
-		return 2
-	end
-
-	local bool_field = mixed_settings[type] or fields.unknown_bool_type
-	subtree:add_le(bool_field, buffer(6,1))
-	return 1
-end
-
-function hist_category(buffer, pinfo,subtree, data_size)
-	local hist_commands = {
-		[0x00] = {fields.hist_deepest_discharge,10},
-		[0x01] = {fields.hist_last_discharge,10},
-		[0x02] = {fields.hist_avrg_discharge,10},
-		[0x03] = {fields.hist_cycles,1},
-		[0x04] = {fields.hist_full_discharge,1},
-		[0x05] = {fields.hist_cum_drawn,10},
-		[0x06] = {fields.hist_min_bat,100},
-		[0x07] = {fields.hist_max_bat,100},
-		[0x08] = {fields.hist_time_full,1},
-		[0x09] = {fields.hist_synch,1},
-		[0x10] = {fields.hist_discharged_energy,100},
-		[0x11] = {fields.hist_charged_energy,100},
-	 }
-	 command = buffer(0,1):le_uint() 
-	 fun = hist_commands[command][1]
-	 if fun == nil then
-		 fun = fields.unknown32
-	 end
-	 local value = buffer(2,data_size):le_int() / hist_commands[command][2] 
-	 subtree:add_le(fun, buffer(2,data_size), value)
-	 return data_size
- end
 
 function single_value(buffer,pinfo,subtree)	
 	if buffer:len() <6 then
@@ -388,7 +258,7 @@ function single_value(buffer,pinfo,subtree)
 	subtree:add_le(fields.data_type, buffer(0,1), data_type)
 
 	local category = buffer(0,4):le_uint()
-	subtree:add_le(fields.command_category, buffer(3,1), category)
+	subtree:add_le(fields.command_category, buffer(3,1), category, data_size, value_commands)
 
 	command_class_nibble = bit.rshift( bit.band(buffer(5,1):uint() , 0xf0) ,4)
 	subtree:add_le(fields.command_class, buffer(5,1), command_class_nibble)
@@ -399,7 +269,7 @@ function single_value(buffer,pinfo,subtree)
 	print("buffer:"..buffer(0,4):le_uint())
 	if buffer(0,4):le_uint() == 0x10190308 then
 		subtree:add_le(fields.settings_value, buffer(4,1))
-		return consumed + settings_category(buffer(4), pinfo, subtree)
+		return consumed + command_category(buffer(4), pinfo, subtree, data_size_nibble, settings_commands)
 	end
 
 	if buffer(0,4):le_uint() == 0x10190309 then
@@ -409,20 +279,19 @@ function single_value(buffer,pinfo,subtree)
 
 	if buffer(0,4):le_uint() == 0xed190308 then
 		subtree:add_le(fields.value, buffer(4,1))
-		return consumed + command_category(buffer(4), pinfo, subtree)
+		return consumed + command_category(buffer(4), pinfo, subtree, data_size_nibble, value_commands)
 	end
 	
 
 	if buffer(0,4):le_uint() == 0x03190308 then
 		subtree:add_le(fields.history, buffer(4,1))
-		return consumed + hist_category(buffer(4), pinfo, subtree, data_size_nibble)
+		return consumed + command_category(buffer(4), pinfo, subtree, data_size_nibble, hist_commands)
 	end
 	
 	if buffer(0,4):le_uint() == 0x0f190308 then
-		subtree:add_le(fields.settings_value, buffer(4,1))
-		return mixed_settings(buffer,pinfo,subtree)
+		subtree:add_le(fields.mixedsettings, buffer(4,1))
+		return command_category(buffer(4),pinfo,subtree, data_size_nibble, mixedsetting_commands)
 	end
-	
 	
 	if  data_type== 0x09 then
 		return add_unknown_bool(buffer(4),pinfo,subtree)
